522d9ab0c885057f0572ae41ff1af1a97df4c9ef
Aradhana Kumari <aradhanakumari2602@gmail.com>
2024-09-15 23:59:57 +0530

    added files navbar,landing page and marquee

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Marquee() {
+  return (
+    <div className='w-full py-10 bg-[#004d43]'>
+        <div className="text border-t-2 bold">
+            <h1>We are ochi</h1>
+            <h1>We are ochi</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Marquee
