e3bed1fbd681616e1d22640e9386bd003e7d7bc3
Aradhana Kumari <aradhanakumari2602@gmail.com>
2024-09-21 18:44:17 +0530

    --

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,13 +11,13 @@ function Cards() {
         </div>
         <div className="cardcontainer flex gap-5 w-1/2 h-[50vh]">
             <div className="card w-1/2 flex relative items-center justify-center rounded-xl h-full bg-[#192826]">
-            <img className='w-32'src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
-            <button className='absolute px-5 py-1 rounded-xl border-2 left-10 bottom-10 '>&copy; 2019-2022</button>
+            <img className='w-32'src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
+            <button className='absolute px-5 py-1 rounded-xl border-2 left-10 bottom-10 '>&copy; RATING 5.0 ON CLUTCH</button>
             </div>
  
             <div className="card w-1/2 flex relative items-center justify-center rounded-xl h-full bg-[#192826]">
-            <img className='w-32'src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
-            <button className='absolute px-5 py-1 rounded-xl border-2 left-10 bottom-10 '>&copy; 2019-2022</button>
+            <img className='w-32 leading-tighter'src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+            <button className='absolute px-5 py-1 rounded-xl border-2 left-10 bottom-10 '>&copy; BUSSINESS BOOTCAMP</button>
             </div>
         </div>
     </div>
