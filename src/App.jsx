0c010be181a84c0612aac2f97984dbad73c6a681
Aradhana Kumari <aradhanakumari2602@gmail.com>
2024-09-21 11:15:59 +0530

    added eye and Featured

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
 import Eyes from './components/Eyes'
+import Featured from './components/Featured'
 function App() {
   return (
     <div className='w-full min-h-screen text-white bg-zinc-900'>
@@ -12,6 +13,7 @@ function App() {
       <Marquee/>
       <About/>
       <Eyes/>
+      <Featured/>
     </div>
   )
 }
---
+++ src/components/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className = 'w-full py-10 bg-zinc-900'>
+        <div className = 'w-full px-10'>
+            <h1 className='text-8xl font-["Neue-Montreal"]'>Featured Projects</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Featured
